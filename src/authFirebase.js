d8a847d845f15df824cca011fe04c27e638c63a3
GuillaumeMaurin <guillaume.maurin@edu.ece.fr>
2018-10-03 20:29:20 +0900

    alert box add

--- src/authFirebase.js
+++ src/authFirebase.js
@@ -2,21 +2,23 @@ function googleLogin() {
    const provider = new firebase.auth.GoogleAuthProvider();
 
    firebase.auth().signInWithPopup(provider).then(result => {
+      // Show user information
       const user = result.user;
       console.log(user);
+      // Change card
       document.getElementById("userName").textContent = user.displayName;
       document.getElementById("emailAdress").textContent = user.email;
       document.getElementById("googlePic").src = user.photoURL;
    }).catch(function(error) {
-      // Handle Errors here.
+      // Handle Errors
       var errorCode = error.code;
       var errorMessage = error.message;
       // The email of the user's account used.
       var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
+      // Show error
       console.log(errorCode);
       console.log(errorMessage);
       console.log(email);
+      alert(errorMessage);
    });
 }
