a4941cd09da3d0f3e009e55ee2069e988bf9f9e5
GuillaumeMaurin <guillaume.maurin@edu.ece.fr>
2018-10-05 01:34:07 +0900

    ecriture sur firebase et init du compte

--- src/authFirebase.js
+++ src/authFirebase.js
@@ -1,5 +1,5 @@
 function googleLogin() {
-   document.getElementById("accountLogo").onclick=null; //evite de clicker plusieur fois dessus
+   document.getElementById("accountLogo").onclick = null; //evite de clicker plusieur fois dessus
    const provider = new firebase.auth.GoogleAuthProvider();
 
    firebase.auth().signInWithPopup(provider).then(result => {
@@ -11,10 +11,11 @@ function googleLogin() {
       document.getElementById("emailAdress").textContent = user.email;
       document.getElementById("googlePic").src = user.photoURL;
       //document.getElementById("accountLogo").src=Content = user.email;
-      document.getElementById("accountLogo").textContent= " ";
-      document.getElementById("accountLogo").style.width="1em";
-      document.getElementById("accountLogo").style.height="1em";
-      document.getElementById("accountLogo").style.backgroundImage= 'url("'+user.photoURL+'")';
+      document.getElementById("accountLogo").textContent = " ";
+      document.getElementById("accountLogo").style.width = "1em";
+      document.getElementById("accountLogo").style.height = "1em";
+      document.getElementById("accountLogo").style.backgroundImage = 'url("' + user.photoURL + '")';
+      initDataUser(user);
    }).catch(function(error) {
       // Handle Errors
       var errorCode = error.code;
@@ -26,6 +27,31 @@ function googleLogin() {
       console.log(errorMessage);
       console.log(email);
       alert(errorMessage);
-      document.getElementById("accountLogo").onclick=googleLogin;//reactive le btn en cas d'err
+      document.getElementById("accountLogo").onclick = googleLogin; //reactive le btn en cas d'err
    });
+}
+
+function initDataUser(user) {
+   console.log(user.uid);
+   // Get a reference to the database service
+   var database = firebase.database();
+   const ref = database.ref('/users/'+user.uid);
+   ref.set({
+      username: user.displayName,
+      email: user.email
+      //some more user data
+   });
+   /*const ref = database.ref('/users/'+user.uid);
+   ref.push({
+      item: "Get Milk",
+      completed: true,
+      archived: true
+   })*/
+   // save the user's profile into Firebase so we can list users,
+   // use them in Security and Firebase Rules, and show profiles
+   /*database.ref('users').set({
+      username: user.name,
+      email: user.email
+      //some more user data
+   });*/
 }
