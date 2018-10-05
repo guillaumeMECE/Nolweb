c53e4192ee894d38bcd155173d7e02b369cdceef
GuillaumeMaurin <guillaume.maurin@edu.ece.fr>
2018-10-05 23:27:43 +0900

    add des cards animation et onglet reservation ainsi que changement et enlevement du calendarview

--- src/reservation.js
+++ src/reservation.js
@@ -1,12 +1,12 @@
-function setupCalendars() {
+/*function setupCalendars() {
    // Embedded Calendar
    /*Calendar.setup({
       dateField: 'embeddedDateField',
       parentElement: 'embeddedCalendar'
    })*/
 
-   // Popup Calendar
-   Calendar.setup({
+// Popup Calendar
+/*Calendar.setup({
       dateField: 'popupDateField',
       triggerElement: 'popupDateField'
    })
@@ -14,24 +14,40 @@ function setupCalendars() {
 
 Event.observe(window, 'load', function() {
    setupCalendars()
-})
+})*/
 
 function tstReservation() {
-   var date = document.getElementById("popupDateField").textContent;
+   var date = document.getElementById("dateField").value;
    console.log(date);
    var heure = document.getElementById("horaire").value;
    console.log(heure);
 
 
    var database = firebase.database();
-   database.ref('/reservationLocal/' + (date + "-" + heure + "-" + (heure + 1))).on('value', function(snapshot) {
+   database.ref('/reservationLocal/' + (date + "-" + heure)).on('value', function(snapshot) {
       if (!(snapshot.exists())) { // tst si première co ou pas
-         const ref = database.ref('/reservationLocal/' + (date + "-" + heure + "-" + (heure + 1)));
+         const ref = database.ref('/reservationLocal/' + (date + "-" + heure));
          ref.set({ // creation et sauvegarde du profil
             reservation: true
          });
-      }else {
-         //alert("impossible");
+      } else {
+         alert("impossible");
       }
    });
+}
+
+function maxCard(objId) {
+   document.getElementById(objId).style.width = "97%";
+   document.getElementById(objId).onclick =  function() {
+      minCard(objId);
+   };;
+   document.getElementById(objId).style.position = "absolute";
+}
+
+function minCard(objId) {
+   document.getElementById(objId).style.width = "30%";
+   document.getElementById(objId).onclick = function() {
+      maxCard(objId);
+   };;
+   document.getElementById(objId).style.position = "static";
 }
